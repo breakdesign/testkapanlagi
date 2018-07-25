70e16a4530f94f4211c65ca547d4831496027dd0
Rizky Maulana <breakdesign28@gmail.com>
2018-07-25 12:55:18 +0700

    Submit Project
    Bismillah

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $(".nav__owl__carousel").owlCarousel({
+        dots: false,
+        autoWidth: true
+    });
+    $(".slider__owl__carousel").owlCarousel({
+        dots: false,
+        items: 1,
+    });
+    $(".trending__topic__owl__carousel").owlCarousel({
+        dots: true,
+        items: 1,
+    });
+    $(".rekomended__topic__owl__carousel").owlCarousel({
+        dots: false,
+        items: 1.8,
+        loop: true,
+        margin: 10,
+    });
+    $('.nav__owl__carousel li a').on('click', function(){
+        $('.nav__owl__carousel li.active').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+    $('.popular__content').infiniteScroll({
+        // options
+        path: 'index-{{#}}.html',
+        append: '.content__populer',
+        history: false,
+        loading: {
+            img: 'img/loader.gif',
+        }
+    });
+    $('.search').click(function() {
+        $(this).toggleClass('on');
+        $('.search_on').slideToggle("slow");
+    });
+});
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+}
